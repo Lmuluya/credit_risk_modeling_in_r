0b951b20abb2c1f9255c6aed03f32c6e299bc67d
Lamech Muluya Kibudde <36754197+Lmuluya@users.noreply.github.com>
2020-09-02 11:11:21 +0300

    Add files via upload

--- introduction_and_data_structure.R
+++ introduction_and_data_structure.R
@@ -1,1 +1,12 @@
-hello
+## Components of expected loss (EL)
+# Probability of default (PD) probability that the borrower will fail to pay the outstanding obligation
+# Exposure at default (EAD) Expected value of te loan at the time of default the amount of loan that is to be repaid
+# Loss given default (LGD) Amount of loss if there is a default
+
+########## Downloading the dataset from the web.
+library(curl)
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://assets.datacamp.com/course/credit-risk-modeling-in-r/loan_data_ch1.rds"
+download.file(fileUrl, destfile = "./data/loan_data.rds", method = "curl")
+loan_data <- readRDS("./data/loan_data_ch1.rds")
+
