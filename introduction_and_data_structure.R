3be5bf7477b19f4525a76e96f149686037d0cb47
Lamech Muluya Kibudde <36754197+Lmuluya@users.noreply.github.com>
2020-09-02 16:24:23 +0300

    Add files via upload

--- introduction_and_data_structure.R
+++ introduction_and_data_structure.R
@@ -70,4 +70,27 @@ hist(loan_data_expert$annual_inc,
      xlab = "Annual Income")
 
 #Bivariate plot
+plot(loan_data$emp_length, loan_data$annual_inc,
+     xlab = "Employment Length", ylab = "Annual Income")
 
+# Creating a  histogram of loan_amnt: hist_1
+hist_1 <- hist(loan_data$loan_amnt)
+
+# Print locations of the breaks in hist_1
+hist_1$breaks
+
+# Change number of breaks and add labels: hist_2
+hist_2 <- hist(loan_data$loan_amnt, breaks = 200, xlab = "Loan amount",
+               main = "Histogram of the loan amount")
+
+# Plot the age variable
+plot(loan_data$age, ylab = "Age")
+
+# Save the outlier's index to index_highage
+index_highage <- which(loan_data$age > 122)
+
+# Create data set new_data with outlier deleted
+new_data <- loan_data[-index_highage, ]
+
+# Make bivariate scatterplot of age and annual income
+plot(loan_data$age, loan_data$annual_inc, xlab = "Age", ylab = "Annual Income")
